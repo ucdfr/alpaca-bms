27077c6cd12b6e52062fdb03ecd605e3d07688d3
khjtony <khjtony@gmail.com>
2015-06-17 20:50:28 -0500

    fix current_sensor

--- alpaca-bms/alpaca-bms.cydsn/current_sense.c
+++ alpaca-bms/alpaca-bms.cydsn/current_sense.c
@@ -14,17 +14,19 @@ void current_init(void)
 
 uint16_t get_current(void)
 {
-	int16_t current = 0;
+	uint16_t raw_vol = 0;
+    float current=0;
 
 	// Delta Sigma measurment of Hall effect sensor
 	ADC_current_StartConvert();
 	ADC_current_IsEndConversion(ADC_current_WAIT_FOR_RESULT);
 	ADC_current_StopConvert();
-	current = ADC_current_GetResult16();
+	raw_vol = ADC_current_GetResult16();
+    
     //todo: convert current to float
 	// current = (Vi - 2.5)/4
 	// Vi referenced to 5V
-	current = (10*(current-25)/4)/10;
+	current = ((10*current-25)/4)/10;
 
     mypack.current = current;
     
