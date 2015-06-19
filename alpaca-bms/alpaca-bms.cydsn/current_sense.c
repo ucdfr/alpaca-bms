23765b475bcedc8b9e44240f298752a49da2db61
J. Tao <jon.g.tao@gmail.com>
2015-06-19 03:56:40 -0500

    revised get_current

--- alpaca-bms/alpaca-bms.cydsn/current_sense.c
+++ alpaca-bms/alpaca-bms.cydsn/current_sense.c
@@ -15,27 +15,33 @@ void current_init(void)
 
 uint16_t get_current(void)
 {
-	uint16_t raw_vol = 0;
-    float current=0;
+	//uint16_t raw_vol = 0;
+	//float current=0;
+	int32_t current;
 
 	// Delta Sigma measurment of Hall effect sensor
+	/*
 	ADC_current_StartConvert();
 	ADC_current_IsEndConversion(ADC_current_WAIT_FOR_RESULT);
 	ADC_current_StopConvert();
 	raw_vol = ADC_current_GetResult16();
-    raw_vol = (5*raw_vol/0xffff);
-    
-    //todo: convert current to float
+	raw_vol = (5*(unsigned)raw_vol/0xffff);
+	*/
+	current = (int32_t)ADC_current_CountsTo_mVolts(ADC_current_Read32());
+
+	//todo: convert current to float
 	// current = (Vi - 2.5)/4
 	// Vi referenced to 5V
-	current = ((10*current-25)/4)/10;
+	//current = ((10*current-25)/4)/10;
+	current = (current - 2500)/4;
 
-    mypack.current = (int16_t)floor(current);
-    
-    //charging?
-    if (current<0){
-    	warning_event |= CHARGEMODE;
-    }
+	//mypack.current = (int16_t)floor(current);
+	mypack.current = (int16_t)current; 
+
+	//charging?
+	if (current<0){
+		warning_event |= CHARGEMODE;
+	}
 	return current;
 }// get_current()
 
